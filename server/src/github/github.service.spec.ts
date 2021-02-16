02e87a4791290916731756a92c9e27a158e62f07
Spencer <43939691+spencer012@users.noreply.github.com>
2021-02-15 23:33:53 -0600

    Added more tests

--- server/src/github/github.service.spec.ts
+++ server/src/github/github.service.spec.ts
@@ -15,6 +15,8 @@ describe('GithubService', () => {
 		}).compile();
 
 		githubService = module.get<GithubService>(GithubService);
+
+		jest.clearAllMocks();
 	});
 
 	it('should be defined', () => {
@@ -23,7 +25,23 @@ describe('GithubService', () => {
 
 	describe('parseGithubLink', () => {
 		it('should return an object that has the relation as the key with the url and page number', () => {
-			expect(githubService.parseGithubLink(commits.parseGithubLink[0])).toEqual(commits.parseGithubLink[1]);
+			expect(githubService.parseGithubLink(commits.parseGithubLink.input)).toEqual(commits.parseGithubLink.output);
+		});
+	});
+
+	describe('getCommits', () => { //if getCommits ever does anything with the commits, the test data will need to be updated
+		it('should return a list of commits', async () => {
+			let mockCommits = jest.fn().mockResolvedValueOnce(commits.getCommits.inputs[0]).mockResolvedValueOnce(commits.getCommits.inputs[1]).mockResolvedValueOnce(commits.getCommits.inputs[2]);
+			//this is for replacing the octonode repo.commitsAsync, it needs this because it gets generated from client.repo
+			githubService.client.repo = (a) => {
+				return {
+					commitsAsync: mockCommits
+				};
+			};
+			
+			let result = await githubService.getCommits({ repo: 1 });
+			expect(result).toEqual(commits.getCommits.output);
+			expect(mockCommits).toHaveBeenCalledTimes(3);
 		});
 	});
 });
