7407b6d7e619f1db92b7a223bb5a27966c63e08f
Spencer <43939691+spencer012@users.noreply.github.com>
2021-02-23 18:17:22 -0600

    Installation script and nginx config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,18 +9,5 @@ module.exports = {
     script: 'node',
     args: './node_modules/@nestjs/cli/bin/nest.js start --watch',
     cwd: 'server'
-  }],
-
-  deploy : {
-    production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
-      'pre-deploy-local': '',
-      'pre-setup': 'sudo apt-get install git; wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash; nvm install',
-      'post-deploy' : 'npm install --prefix ./server ./; npm install --prefix ./client ./; pm2 reload ecosystem.config.js --env production',
-    }
-  }
+  }]
 };
