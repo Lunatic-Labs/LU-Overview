01ad10343a5e521b3a4a4888d5582d62d408fddd
Negar Jafari <54953465+nejafari@users.noreply.github.com>
2021-03-02 22:30:20 -0600

    Sidebar and Dashboard changes made

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,11 +7,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import {MatExpansionModule} from '@angular/material/expansion';
 import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
 	declarations: [
 		AppComponent,
-		LoginComponent
+		LoginComponent,
+		DashboardComponent
 	],
 	imports: [
 		BrowserModule,
