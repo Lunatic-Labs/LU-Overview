d243ae245a6dd0a37140a00d56a412e4798e31cf
Min Kim <minkim834@gmail.com>
2021-03-08 15:54:52 -0600

    Graph Implimentation

--- client/src/app/dashboard/dashboard.component.ts
+++ client/src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var Chart:any;
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -12,6 +13,146 @@ export class DashboardComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+      this.createChart('myChart');
+
+  }
+
+  createChart(id:string): void {
+      let temp:any = document.getElementById('myChart');
+      let temp1:any = document.getElementById('myChart1');
+      let myChart = temp.getContext('2d');
+      let myChart1 = temp1.getContext('2d');
+
+// Start of "Total Productivity Chart"
+Chart.defaults.global.defaultFontFamily = 'Lato';
+Chart.defaults.global.defaultFontSize = 18;
+Chart.defaults.global.defaultFontColor = '#777';
+
+let massPopChart = new Chart(myChart, {
+  type:'doughnut', // bar, horizontalBar, pie, line, doughnut, radar, polarArea
+  data:{
+    labels:['Jibble', 'Github', 'Slack', 'Meetings', 'Others'],
+    datasets:[{
+      label:'Percentage',
+      data:[
+        20,
+        20,
+        20,
+        20,
+        20
+      ],
+      //backgroundColor:'green',
+      backgroundColor:[
+        'rgba(255, 99, 132, 0.6)',
+        'rgba(54, 162, 235, 0.6)',
+        'rgba(255, 206, 86, 0.6)',
+        'rgba(75, 192, 192, 0.6)',
+        'rgba(153, 102, 255, 0.6)',
+        'rgba(255, 159, 64, 0.6)',
+        'rgba(255, 99, 132, 0.6)'
+      ],
+      borderWidth:1,
+      borderColor:'#777',
+      hoverBorderWidth:3,
+      hoverBorderColor:'#000'
+    }]
+  },
+  options:{
+    title:{
+      display:true,
+      text:'Total Productivity',
+      fontSize:25
+    },
+    legend:{
+      display:true,
+      position:'right',
+      labels:{
+        fontColor:'#000'
+      }
+    },
+    layout:{
+      padding:{
+        left:50,
+        right:0,
+        bottom:0,
+        top:0
+      }
+    },
+    tooltips:{
+      enabled:true
+    }
+  }
+});
+// END of "Total Productivity Chart"
+
+// Start of Jibble
+
+
+// Global Options
+Chart.defaults.global.defaultFontFamily = 'Lato';
+Chart.defaults.global.defaultFontSize = 18;
+Chart.defaults.global.defaultFontColor = '#777';
+
+let massPopChart1 = new Chart(myChart1, {
+  type:'horizontalBar', // bar, horizontalBar, pie, line, doughnut, radar, polarArea
+  data:{
+    labels:['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+    datasets:[{
+      label:'Hours',
+      data:[
+        2.15,
+        3,
+        6,
+        1.45,
+        4.8,
+        5,
+        2
+      ],
+      //backgroundColor:'black',
+      backgroundColor:[
+        'rgba(255, 99, 132, 0.6)',
+        'rgba(255, 99, 132, 0.6)',
+        'rgba(255, 99, 132, 0.6)',
+        'rgba(255, 99, 132, 0.6)',
+        'rgba(255, 99, 132, 0.6)',
+        'rgba(255, 99, 132, 0.6)',
+        'rgba(255, 99, 132, 0.6)'
+
+      ],
+
+      borderWidth:1,
+      borderColor:'#777',
+      hoverBorderWidth:3,
+      hoverBorderColor:'#000'
+    }]
+  },
+  options:{
+    title:{
+      display:true,
+      text:'Jibble Time',
+      fontSize:25
+    },
+    legend:{
+      display:true,
+      position:'right',
+      labels:{
+        fontColor:'#000'
+      }
+    },
+    layout:{
+      padding:{
+        left:50,
+        right:0,
+        bottom:0,
+        top:0
+      }
+    },
+    tooltips:{
+      enabled:true
+    }
+  }
+});
+// End of JIbble chart
   }
 
 }
