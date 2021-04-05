28edf631c24673bee00d73d029c8034971c9ca8f
santosalvarez <salvarezgarza@mail.lipscomb.edu>
2021-04-04 20:01:58 -0500

    npm installed passport-google-oauth

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
+var passport = require('passport');
+
 async function bootstrap() {
 	const app = await NestFactory.create<NestExpressApplication>(AppModule);
 	await app.listen(3000);
