d7cb0f6919263bf69657be94b906d607426a918a
santosalvarez <salvarezgarza@mail.lipscomb.edu>
2021-04-11 16:14:17 -0500

    Added some implementation steps, included source

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { userInfo } from 'os';
 import { AppModule } from './app.module';
-
 
 
 async function bootstrap() {
@@ -10,28 +10,45 @@ async function bootstrap() {
 }
 bootstrap();
 
-var passport = require('passport')
+const passport = require('passport')
 const express = require('express')
 const app = express()
+const keys = require('./keys.js');
 
-var GoogleStrategy = require('passport-google-oauth20').Strategy;
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 //This is the template // Not currently functional
 passport.use(new GoogleStrategy({
-    clientID: GOOGLE_CLIENT_ID,
-    clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://www.example.com/auth/google/callback"
+    clientID: keys.google.clientID,
+    clientSecret: keys.google.clientSecret,
+    callbackURL: "/auth/google/callback"
   },
-  function(accessToken, refreshToken, profile, cb) {
-    User.findOrCreate({ googleId: profile.id }, function (err, user) {
-      return cb(err, user);
-    });
-  }
+  // Use this when working with a database
+  //function(accessToken, refreshToken, profile, cb) {
+  //  User.findOrCreate({ googleId: profile.id }, function (err, user) {
+  //    return cb(err, user);
+  //  });
+  //}
 ));
 
 ///Define Routes///
-app.get('/login',
-  function(req, res){
-    res.render('login');
-  });
+app.get('/auth/google', passport.authenticate("google", {
+    scope: ['profile', 'email']
+}));
+
+app.get("/auth/google/callback",passport.authenticate('google'));
+
 ///////////////////
+
+//Keep track of logged in status if Google is not used
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+// passport.deserializeUser((id, done) => {
+//   User.findById(id).then(user => {
+//     done(null, user);
+//   });
+// });
+
+// Source: https://dev.to/phyllis_yym/beginner-s-guide-to-google-oauth-with-passport-js-2gh4 //
