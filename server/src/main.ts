afe0d7aa48fee5823e9397bc2741714617d95dd3
santosalvarez <salvarezgarza@mail.lipscomb.edu>
2021-04-11 15:29:43 -0500

    rearanged some lines and changed var's to const's

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,10 +2,36 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
-var passport = require('passport');
+
 
 async function bootstrap() {
 	const app = await NestFactory.create<NestExpressApplication>(AppModule);
 	await app.listen(3000);
 }
 bootstrap();
+
+var passport = require('passport')
+const express = require('express')
+const app = express()
+
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+//This is the template // Not currently functional
+passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://www.example.com/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+///Define Routes///
+app.get('/login',
+  function(req, res){
+    res.render('login');
+  });
+///////////////////
